22e11c4ec801d919109d8baef28c0a517167bd27
PH Saurav <phsaurav29@gmail.com>
2022-03-08 03:08:37 +0600

    Postgresql Added

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Portfolio_db',
+        'USER': 'postgres',
+        'PASSWORD': 'macbook29',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
