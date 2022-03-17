802856203eca735d139d31a5a7c83c771528f71f
PH Saurav <phsaurav29@gmail.com>
2022-03-17 23:17:56 +0600

    Home Edited

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return (
-    <div >
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline'> This is the home page!</h1>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
