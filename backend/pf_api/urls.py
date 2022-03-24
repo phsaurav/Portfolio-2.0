c8cd683ff4bbe12b3a30b74a9883edea476b1282
PH Saurav <phsaurav29@gmail.com>
2022-03-24 23:43:00 +0600

    Converted to APIView

--- backend/pf_api/urls.py
+++ backend/pf_api/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
-from rest_framework import routers
+# from rest_framework import routers
 from pf_api import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'project', views.AllProjects)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'project', views.AllProjects)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework'))
+    path('projects/', views.ProjectList.as_view(), name='projects'),
+    path('projects/<int:pk>', views.ProjectDetail.as_view(), name='project'),
+    # path('', include(router.urls)),
+    # path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/pf_api/views.py
+++ backend/pf_api/views.py
@@ -2,19 +2,43 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import UserSerializer, ProjectSerializer
 from .models import Project
+from django.views.generic.list import ListView
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
-class AllProjects(viewsets.ModelViewSet):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [permissions.AllowAny]
+# class AllProjects(viewsets.ModelViewSet):
+#     queryset = Project.objects.all()
+#     serializer_class = ProjectSerializer
+#     permission_classes = [permissions.AllowAny]
+
+class ProjectList(APIView):
+    
+    def get(self, request, format=None):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+
+
+class ProjectDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
